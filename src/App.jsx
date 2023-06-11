e5d50843fc76007100a0fc015654ffaa02570898
DautSadykov <sadykov.daut@yandex.ru>
2023-06-11 13:58:24 +0300

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,19 @@ import { DarkMode } from "@mui/icons-material";
 export default function App() {
   const [darkmode, setDarkMode] = useState(true)
 
+  function toggleDarkMode () {
+    console.log('1')
+    setDarkMode(prev => !prev)
+  }
+
   
 
   return (
     <div className="relative overflow-hidden w-screen">
       <img className="hero_img_sm" src="hero_image_sm.png"/>    
-      <div className={`darkmode_bg ${darkmode ? 'absolute' : 'hidden'} absolute top-0 -z-20 bg-gray-900 w-screen h-[900px] sm:h-[600px]`}></div>  
+      <div className={`darkmode_bg ${darkmode ? 'absolute' : 'hidden'} absolute top-0 -z-20 bg-gray-900 w-screen h-[900px] sm:h-[600px] transition-all duration-1000`}></div>  
       <div className="main p-5">
-        <HeroSection darkmode={darkmode} />
+        <HeroSection darkmode={darkmode} toggleDarkMode={toggleDarkMode}/>
         <TabsSection darkmode={darkmode} />
         <SubscriptionSection />
       </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,24 +1,48 @@
+import { ConstructionOutlined, DarkMode, LightMode } from "@mui/icons-material";
 import React from "react";
 
-export default function HeroSection({darkmode}) {
+export default function HeroSection({ darkmode, toggleDarkMode }) {
   return (
     <div className="relative">
-      <img className="hero_img_lg" src="hero_image_lg.png"/>
-      <img className="hero_img_md" src="hero_image_md.png"/>
-      <p className={`text-gray-500 mb-5 text-sm lg:mb-10 max-sm:hidden ${darkmode && "darkmode_text"}`}>
-        Home {">"} Sub Page 2 {">"} Sub Page 3 {">"} Sub Page 4
-      </p>
-      <div>
-        <button className="hero_button p-1 px-10 rounded-full font-bold lg:mb-5">
-          If you need an Special Offer Block
-        </button>
-        
+      <img className="hero_img_lg" src="hero_image_lg.png" />
+      <img className="hero_img_md" src="hero_image_md.png" />
+      <div className="flex items-center gap-5 mb-5 lg:mb-10 ">
+        <p
+          className={`text-gray-500 text-sm max-sm:hidden ${
+            darkmode && "darkmode_text"
+          }`}
+        >
+          Home {">"} Sub Page 2 {">"} Sub Page 3 {">"} Sub Page 4
+        </p>
+        <div
+          className={`${darkmode ? "hidden" : "block"}`}
+          onClick={toggleDarkMode}
+        >
+          <DarkMode className="hidden" style={{ color: "black" }} />
+        </div>
+        <div
+          className={`${darkmode ? "block" : "hidden"}`}
+          onClick={toggleDarkMode}
+        >
+          <LightMode style={{ color: "white" }} />
+        </div>
       </div>
-      <h1 className={`w-1/2 my-5 text-3xl font-light tracking-wider ${darkmode && "darkmode_text"}`}>
+      <button className="hero_button p-1 px-10 rounded-full font-bold lg:mb-2 ">
+        If you need an Special Offer Block
+      </button>
+      <h1
+        className={`w-1/2 my-5 text-3xl font-light tracking-wider ${
+          darkmode && "darkmode_text"
+        }`}
+      >
         <b className="font-bold">Lorem ipsm</b> dolor, sit amet
       </h1>
       <div className="h-1 w-20 border-b border-gray-400"></div>
-      <p className={`w-3/5 lg:w-2/5 lg:mb-5 text-gray-500 ${darkmode && "darkmode_text"}`}>
+      <p
+        className={`sm:w-3/5 lg:w-2/5 lg:mb-5 text-gray-500 ${
+          darkmode && "darkmode_text"
+        }`}
+      >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dapibus
         eleifend mi eu faucibus. Aliquam erat volutpat. Pellentesque dui nisl,
         consectetur sit amet hendrerit at, commodo ac nunc.
